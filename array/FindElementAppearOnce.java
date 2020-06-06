04b01810e6d9a128cd4af074d3a72985cfdb17ed
mohitmaddhesiya <mohitmaddhesiya@gmail.com>
2020-06-06 23:03:59 +0530

    added Bit wise approach to find appear once in array

--- array/FindElementAppearOnce.java
+++ array/FindElementAppearOnce.java
@@ -7,13 +7,13 @@
  * Input: arr[] = {10, 20, 10, 30, 10, 30, 30} Output: 20 In the given array all
  * element appear three times except 20 which appears once.
  */
-package array;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public class FindElementAppearOnce {
+    static final int INT_SIZE = 32; 
     // brute force Approach
     // set negtive with find
     static int bruteForce(int arr[]) {
@@ -75,7 +75,6 @@ public class FindElementAppearOnce {
         }
         Collections.sort(al);
         int count = 0;
-        System.out.println(" al " + al);
         for (int i = 1; i < n; i++) {
             if (al.get(i - 1) == al.get(i)) {
                 count++;
@@ -91,12 +90,61 @@ public class FindElementAppearOnce {
         }
         return result;
     }
-    
+    /**
+     * We can sum the bits in same positions for all the numbers and take modulo with 3. 
+     * The bits for which sum is not multiple of 3, are the bits of number with single occurrence.
+        Let us consider the example array 
+                        {5, 5, 5, 8}. 
+                    The 101, 101, 101, 1000
 
+        Sum of first bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+        Sum of second bits%3 = (0 + 0 + 0 + 0)%0 = 0;
+        Sum of third bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+        Sum of fourth bits%3 = (1)%3 = 1;
+        Hence number which appears once is 1000
+     * @param arr
+     * @param n
+     * @return
+     */
+    // Method to find the element that occur only once 
+    static int getSingle(int arr[], int n) 
+    { 
+        int result = 0; 
+        int x, sum; 
+          
+        // Iterate through every bit 
+        for(int i=0; i<INT_SIZE; i++) 
+        { 
+            // Find sum of set bits at ith position in all  
+            // array elements 
+            sum = 0; 
+            x = (1 << i); 
+            for(int j=0; j<n; j++) 
+            { 
+                // arr[j] & x result will be x
+                // e.g x=2 and arr[j] then 
+                // arr[j] & x= 2 & 2 = 11 & 11 (bit wise caculation) = 11 (bitwise result ) = 2(convert in int)  
+                if((arr[j] & x) == x) 
+                    sum++; 
+            } 
+            // The bits with sum not multiple of 3, are the 
+            // bits of element with single occurrence. 
+            if ((sum % 3) != 0) 
+                result |= x; 
+        } 
+        return result; 
+    }
     public static void main(String args[]) {
-        int arr[] = { 12, 1, 12, 3, 12, 1, 1, 2, 3, 3, 2, 2, 10 };
+        int arr[] = {10, 20, 10, 30, 10, 30, 30};
+        int n =arr.length;
+        int copy[]={10, 20, 10, 30, 10, 30, 30};
+        for(int i=0;i<n;i++)
+            System.out.print(" \t " +arr[i]);
+        System.out.println("");
         System.out.println(" broute force approach " + bruteForce(arr));
+        arr=copy;
         System.out.println(" HashMap approach " + hashmap(arr));
         System.out.println(" Sorting Approach " + sortingApproach(arr));
+        System.out.println(" Best Approach using Bit Count " + getSingle(arr, n));
     }
 }
